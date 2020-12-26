2f2ff725c5cf09463293076ee8bf8e5b80d9cf73
Bui Nam <nambk@bravebits.vn>
2020-12-26 18:05:21 +0700

    first commit

---
+++ src/components/ShitOne.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function ShitOne() {
+    return (
+        <div style={{backgroundColor: 'red', color: 'white',}}>
+            shit one
+        </div>
+    )
+}
