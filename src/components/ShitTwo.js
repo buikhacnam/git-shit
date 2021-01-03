9485f2a0ad304340250daab89c1fd6944beb0d7c
Bui Nam <nambk@bravebits.vn>
2021-01-03 20:35:22 +0700

    add shit two

---
+++ src/components/ShitTwo.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function ShitTwo() {
+    return (
+        <div style={{backgroundColor: "black", color: 'white'}}>
+            Shit Two
+        </div>
+    )
+}
