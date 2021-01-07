a070282a05fb5b5ba32e8742aceaeee54b9404e8
Bui Nam <nambk@bravebits.vn>
2021-01-07 21:59:44 +0700

    add git six finally

--- src/App.js
+++ src/App.js
@@ -3,11 +3,8 @@ import ShitOne from './components/ShitOne';
 import ShitTwo from './components/ShitTwo';
 import ShitThree from './components/ShitThree';
 import ShitFour from './components/ShitFour';
-<<<<<<< HEAD
 import ShitSix from './components/ShitSix';
-=======
 import ShitFive from './components/ShitFive';
->>>>>>> 906cdae68d3b5dcb2e527b949fbbb0e9d4194da6
 
 function App() {
   return (
@@ -17,11 +14,8 @@ function App() {
       <ShitTwo />
       <ShitThree />
       <ShitFour />
-<<<<<<< HEAD
-      <ShitSix />
-=======
       <ShitFive />
->>>>>>> 906cdae68d3b5dcb2e527b949fbbb0e9d4194da6
+      <ShitSix />
     </div>
   );
 }
